20dec943989e36df1bfe6a1b34d23e8eb9cbb273
sandreza <andrenogueirasouza@gmail.com>
2021-12-13 11:19:09 -0500

    sketch emulator a bit more

--- src/Emulate/emulator.jl
+++ src/Emulate/emulator.jl
@@ -43,4 +43,74 @@ struct ConditionalMean{S}
 end
 struct ConditionalCovariance{S}
 end
+
+factorize(x, y::Nothing) = x
+factorize(x, ::Cholesky) = cholesky(x)
+
+# add in sparse cholesky, example on extending
+SparseCholesky() = SparseCholesky(robust = true, threshold = 0.2)
+function factorize(K, factorization::SparseCholesky)
+        mK = maximum(K)
+        (; robust, threshold) = factorization
+        # make Cholesky factorization robust 
+        # by adding a small amount to the diagonal
+        if robust
+            K += mK*sqrt(eps(1.0))*I
+        end
+        # check sparsity
+        bools = K .> entry_threshold * mK
+        sparsity = sum(bools) / length(bools)
+
+        # use sparse cholesky if below threshold
+        # otherwise return normal 
+        if sparsity < sparsity_threshold
+            sparse_K = similar(K) .* 0
+            sparse_K[bools] = sK[bools]
+            K = sparse(Symmetric(sparse_K))
+            CK = cholesky(K)
+        else
+            CK = cholesky(K)
+        end
+end
+
+
+function condition(gp::GaussianProcess, data::FormatA; factorization = nothing )
+    (; X, Y) = data
+    # factorize once since it shows up in both
+    # the mean and covariance
+    K = cov(gp, X)
+    K = factorize(K, factorization)
+    cond_cov  = ConditionalCovariance(gp, X, K)
+    cond_mean = ConditionalMean(gp, X, K)
+    return GaussianProcess(mean = cond_mean, covariance = cond_cov)
+end
+# this
+function ConditionalCovariance(gp, X, K)
+    function cov(x, y)
+        cX = [gp.k(x, dx) for dx in X]
+        cY = [gp.k(y, dx) for dx in X]
+        var = gp.k(x, y) .- cX' * (K \ cY)
+    end
+end
+# OR 
+# the nice part about this way is we have a record
+# of the data that has been seen
+struct ConditionalCovariance{GP, DATA, MAT}
+    k::GP
+    X::DATA
+    K::MAT
+end
+function (cc::ConditionalCovariance)(x,y)
+        cX = [cc.k(x, dx) for dx in cc.X]
+        cY = [cc.k(y, dx) for dx in cc.X]
+        var = cc.k(x, y) - cX' * (cc.K \ cY)
+end
+# for 1 layer of data
+# gp.covariance.gp.covariance
+# for 2 layer of data
+# gp.covariance.gp.covariance.gp.covariance 
+# could probably define
+original_covariance(covariance::ConditionalCovariance) = original_covariance(covariance.gp)
+original_covariance(covariance::Function) = covariance
+# jumpin through the tree is also possible
 =#
